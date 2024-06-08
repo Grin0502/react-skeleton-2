e479208defdfadbad9f0dc2d42f497da1d81cbe8
James Brian <JamesBrianSm@outlook.com>
2024-06-08 11:01:44 -0700

    layout

---
+++ src/layout/client/footer.tsx
@@ -0,0 +1,12 @@
+
+const Footer = () => {
+    return (
+        <div className="w-full h-[200px] bg-gray-500">
+            <div className="flex justify-center items-center color-gray text-[50px] max-w-[1200px] h-full m-auto">
+                Footer
+            </div>
+        </div>
+    );
+};
+
+export default Footer;
---
+++ src/layout/client/index.tsx
@@ -0,0 +1,15 @@
+import { Outlet } from "react-router-dom";
+import Header from "./header";
+import Footer from "./footer";
+
+const ClientLayout = () => {
+  return (
+    <>
+      <Header />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
+
+export default ClientLayout;
